4b8ec02585e5b3e36f3ef719183d35a9ce6e761e
HuzaifaMk <76698805+HuzaifaMk@users.noreply.github.com>
2022-07-18 15:55:59 +0500

    FInal release

--- cmd/open.go
+++ cmd/open.go
@@ -1,9 +1,11 @@
 package cmd
 
 import (
-	helper "cdrawer/helpers"
+	"encoding/hex"
 	"fmt"
+	"log"
 
+	"github.com/jacobsa/go-serial/serial"
 	"github.com/spf13/cobra"
 )
 
@@ -13,9 +15,37 @@ var openCmd = &cobra.Command{
 	Use:   "open 1234",
 	Short: "Open Cash Drawer on specific serial port",
 	Run: func(cmd *cobra.Command, args []string) {
-		s := helper.HexToBytes(args[0])
-		fmt.Println("Conversion to Bytes successful: ", s)
-		fmt.Println("Open", port)
+		data, err := hex.DecodeString("01020304")
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("Conversion to Bytes successful: ")
+		fmt.Printf("% x \n", data)
+		fmt.Println("Opening the port: ", port)
+		fmt.Println("The drawer is opening...")
+
+		options := serial.OpenOptions{
+			PortName:   "COM1",
+			BaudRate:   9600,
+			DataBits:   8,
+			StopBits:   1,
+			ParityMode: serial.PARITY_NONE,
+		}
+
+		port, err := serial.Open(options)
+		if err != nil {
+			log.Fatalf("serial.Open: %v", err)
+		}
+
+		defer port.Close()
+
+		b := []byte{0x0c, 0x01, 0x02, 0x03, 0x04, 0x00, 0x00, 0x00}
+		n, err := port.Write(b)
+		if err != nil {
+			log.Fatalf("port.Write: %v", err)
+		}
+
+		fmt.Println("Wrote", n, "bytes.")
 
 	},
 }
--- go.mod
+++ go.mod
@@ -2,9 +2,13 @@ module cdrawer
 
 go 1.18
 
-require github.com/spf13/cobra v1.5.0
+require (
+	github.com/jacobsa/go-serial v0.0.0-20180131005756-15cf729a72d4
+	github.com/spf13/cobra v1.5.0
+)
 
 require (
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
 )
