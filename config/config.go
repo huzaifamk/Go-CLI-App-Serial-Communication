d88fca548308bf2c4e9e8cb9b6717425d50b3399
HuzaifaMk <76698805+HuzaifaMk@users.noreply.github.com>
2022-07-18 23:23:45 +0500

    Build for windows 32 bit

--- config/config.go
+++ config/config.go
@@ -11,6 +11,19 @@ var P = cfg.Section("").Key("PortName").String()
 var B, _ = cfg.Section("").Key("BaudRate").Uint()
 var D, _ = cfg.Section("").Key("DataBits").Uint()
 var S, _ = cfg.Section("").Key("StopBits").Uint()
+var Pm, _ = cfg.Section("").Key("ParityMode").Uint()
+
+func CheckPm(x uint) serial.ParityMode {
+	switch Pm {
+	case 0:
+		return serial.PARITY_NONE
+	case 1:
+		return serial.PARITY_ODD
+	case 2:
+		return serial.PARITY_EVEN
+	}
+	return serial.PARITY_NONE
+}
 
 // Here you can edit your configurations, you can also make new configurations and apply it to any of the command, just by changing the name of var <Options> in your cmd files.
 var Options = serial.OpenOptions{
@@ -18,7 +31,7 @@ var Options = serial.OpenOptions{
 	BaudRate:   B,
 	DataBits:   D,
 	StopBits:   S,
-	ParityMode: serial.PARITY_NONE,
+	ParityMode: CheckPm(Pm),
 }
 
 //Here is a new sample configuration, you can uncomment and use this if you want to and edit it :D
